851f805a4921080510e487ae13d8998ed173fcc0
Secret <167507314+virusgroup@users.noreply.github.com>
2024-09-28 20:36:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
 global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923300795089'] //ur owner number, dont add more than one
+global.ownernumber = ['923188211549'] //ur owner number, dont add more than one
 global.ownername = '© GlobalTechInfo' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
@@ -19,9 +19,9 @@ global.wm = "Global Bot Inc."
 global.botscript = 'https://github.com/virusgroup/GLOBAL-MD.git' //script link
 global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.creator = "923188211549@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["923188211549"] // Premium User
 
 //channel id
 global.xchannel = {
