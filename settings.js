5722ee42e3842b6b8884abfa10776f7ef6bec8c4
sarwarali123321 <sarwaraliking706@gmail.com>
2024-09-29 13:50:07 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
 global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923188211549'] //ur owner number, dont add more than one
+global.ownernumber = ['923300795089'] //ur owner number, dont add more than one
 global.ownername = '© GlobalTechInfo' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
